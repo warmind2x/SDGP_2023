999d71ca30de50138e861052c96592e56cd10a39
“[firstname <jgarban@outlook.com>
2023-05-20 13:27:47 -0400

    Register & Login complete Back

--- api/routes/Users.js
+++ api/routes/Users.js
@@ -36,6 +36,39 @@ router.post("/register", async (req, res) =>{
         console.error(error.errors[0]);
         res.status(404).json(error.errors[0].message);
     }
+});
+
+router.get("/login", async (req, res) =>{
+    try {
+        console.log(req.query)
+        const logUser = {
+            userId: req.query.userId,
+            password: req.query.password
+        };
+
+        let user = await User.findOne({where:{userId: logUser.userId}});
+        console.log(user.dataValues)
+        if (!user) {
+            const response = {
+                status: "User does´t exist"
+            };
+            return res.status(401).json(response);
+         
+
+
+        } else {
+            const toSend = user.dataValues;
+            if (bcrypt.compareSync(logUser.password,user.dataValues.password)) {
+                
+                console.log(user.dataValues.nombre);
+                return res.status(200).json(toSend)
+            };
+            
+        }
+    } catch (error) {
+        console.error(error.errors[0]);
+        res.status(404).json(error.errors[0].message);
+    }
 })
 
 module.exports = router;
